154bb7fc03499ca8addaa53ac08a4779b1dcaf3d
Anil Madhavapeddy <anil@recoil.org>
2014-05-12 15:39:40 +0100

    scripts to build more customized docker images

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -ex
+
+distro=$1
+version=$2
+
+cp scripts/$distro/opam-installext .
+cp scripts/$distro/Dockerfile .
+
+case $distro,$version in 
+ubuntu,4.01.*)
+  ;;
+ubuntu,4.02.*)
+  echo "RUN opam switch 4.02.0dev+trunk" >> Dockerfile
+  ;;
+*)
+  echo Unknown $distro $version combo
+  exit 1
+  ;;
+esac
+
+sudo docker.io build --no-cache=true -t opam:$distro-$version .
