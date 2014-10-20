eb46962cde83c5a3ebbb297239873e1e3c901eb4
Anil Madhavapeddy <anil@recoil.org>
2014-10-19 17:49:42 -0700

    Also add an archival target for `opam-admin archive` serving

--- sync.sh
+++ sync.sh
@@ -6,3 +6,8 @@ for v in 4.01.0 4.02.1; do
     ./setup.sh $d $v
   done
 done
+
+# Build the archive daemon over Ubuntu Trusty
+rm -rf build/ubuntu-trusty-archive
+cp -r build/ubuntu-trusty-4.02.1 build/ubuntu-trusty-archive
+cat scripts/ubuntu-trusty/Dockerfile.archive >> build/ubuntu-trusty-archive/Dockerfile
