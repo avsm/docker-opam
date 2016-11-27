e3c24a0736c36ed2413e40ca7a6d679ead92b0f5
Marek Kubica <marek@xivilization.net>
2016-11-27 12:50:22 +0100

    Use a newer patch release of 4.02, 4.02.3

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 # Generate all Dockerfiles
 
-for v in 4.01.0 4.02.1 4.03.0 4.04.0 4.05.0; do
+for v in 4.01.0 4.02.3 4.03.0 4.04.0 4.05.0; do
   for d in debian-wheezy debian-jessie ubuntu-trusty centos-7 centos-6; do
     ./setup.sh "$d" "$v"
   done
